0e02fcf5a990793769470c7f8bc9e1305aad2dec
miguelbacalhau <miguelnjacinto@gmail.com>
2025-05-23 23:23:45 +0200

    [vim] modernize config/plugins

---
+++ lazyvim/lua/plugins/ui.lua
@@ -0,0 +1,35 @@
+return {
+	-- colorscheme
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		config = function()
+			vim.cmd.colorscheme("onedark")
+		end,
+	},
+	-- status line
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = "onedark",
+					component_separators = { "", "" },
+					section_separators = { "", "" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = { "filename" },
+					lualine_x = { "filetype" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+				},
+			})
+		end,
+	},
+}
