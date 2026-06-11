6d2b074ee44720f29fd18428f887f9e1fa8a828b
miguelbacalhau <miguelnjacinto@gmail.com>
2026-06-11 17:35:35 +0200

    [planck] update keymap

--- plank/keymap.c
+++ plank/keymap.c
@@ -34,14 +34,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  | GUI  | Alt  | Caps |Lower |    Space    |Raise | Alt  | GUI  | Ctrl |Shift |
+ * | Tab  | GUI  | Alt  | Caps |Lower |    Space    |Raise | Left | Down | Up   |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_TAB,  KC_LGUI, KC_CAPS, KC_LALT, LOWER, KC_RSFT, KC_SPC, RAISE, KC_RALT, KC_RGUI, KC_RCTL, KC_RSFT
+    KC_TAB,  KC_LGUI, KC_CAPS, KC_LALT, LOWER, KC_RSFT, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
 ),
 
 
@@ -49,18 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  | Left | Down |  Up  |Right |      |      |
+ * |      |      |      |      |      |      | Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Next | Vol- | Vol+ |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  | Nex  | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_MNXT, KC_VOLD, KC_VOLU, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,_______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_MNXT, KC_VOLD , KC_VOLU, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______
 ),
 
 /* Raise
